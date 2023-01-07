2099239855e9e9e1cbf43b7d5aef13eaf309756d
abdoessordo <abdoessordo01@gmail.com>
2023-01-08 00:34:37 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
+import {
+  TranslateClient,
+  CreateParallelDataCommand,
+} from "@aws-sdk/client-translate";
+
 function App() {
+  // a client can be shared by different commands.
+  const client = new TranslateClient({ region: "REGION" });
+
+  const params = {
+    /** input parameters */
+  };
+  const command = new CreateParallelDataCommand(params);
+
+  client.send(command).then(
+    (data) => {
+      // process data.
+    },
+    (error) => {
+      // error handling.
+    }
+  );
+
   return <div>RAWAJ</div>;
 }
 
